fefba23aed5e21e61f5c9b57d409224f22ae395f
SreenithiRamesh <sreenithiramesh2@gmail.com>
2026-09-11 11:37:36 +0530

    chore: initialize ExpenseMate Spring Boot backend

---
+++ src/test/java/com/expensemate/ExpensemateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensemateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
